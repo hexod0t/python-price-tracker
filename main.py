728891079a2b73f63f1d80bc2db59753044b81ed
hexod0t <cris_does@hotmail.es>
2020-12-01 12:10:23 -0800

    Add Menu options and define functions for each one

--- main.py
+++ main.py
@@ -11,12 +11,32 @@ from time import sleep
 
 symbols = "AMZN GOOG NFLX FB GLD SPY".split()
 
+options = "Track Default List, Show Default List, \
+    Add to Default, Edit Default List, Add new List, \
+        Quit".split(",")
+
+def show_default():
+    pass
+
+def add_to_default():
+    pass
+
+def edit_default():
+    pass
+
+def add_list():
+    pass
+
 def get_prices(symbols):
     symbols.sort()
     return pdr.get_quote_yahoo(symbols)['price']
 
 def main():
-    while True:
+    run_program = True
+    while run_program:
+        print("Choose Options")
+        for i in range(len(options) + 1):
+            print("{} - {}".format(i, options[i-1]))
         print(get_prices(symbols))
         print("CNTL + C to quit")
         sleep(5)
